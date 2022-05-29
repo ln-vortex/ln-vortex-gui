2402525a2810423eff8b5d8a0d5fbc5f2a0c5c3f
deregs <deregs@protonmail.com>
2022-05-29 08:14:43 -0400

    add styling

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import '../styles/styles.css';
+import '../styles/fonts.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/_document.jsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/IBMPlexMono-Regular.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

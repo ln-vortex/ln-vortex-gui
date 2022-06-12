2d9703eb84d23cb3c87ddddf960220c5a5545ebc
deregs <deregs@protonmail.com>
2022-06-12 15:25:16 -0400

    add ability to cancel channel open

--- pages/status.js
+++ pages/status.js
@@ -1,13 +1,33 @@
-import useSWR from 'swr';
+import { useState } from 'react';
+import useSWR, { useSWRConfig } from 'swr';
 import Header from '../components/Header';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Index() {
   const { data: statusData, error } = useSWR('/api/status', fetcher);
+  const [cancelCoinsError, setCancelCoinsError] = useState('');
+  const [cancelCoinsLoading, setCancelCoinsLoading] = useState(false);
+  const { mutate } = useSWRConfig();
+
+  const handleCancelCoins = async () => {
+    setCancelCoinsError('');
+    setCancelCoinsLoading(true);
+
+    const response = await fetch('/api/cancelcoins');
+    const data = await response.json();
+
+    if (data.error) {
+      setCancelCoinsError(data.error);
+    } else {
+      mutate('/api/status');
+    }
+
+    setCancelCoinsLoading(false);
+  };
 
   if (error) return <div>Failed to load</div>;
-  if (!statusData) return <div>Loading...</div>;
+  if (!statusData || cancelCoinsLoading) return <div>Loading...</div>;
 
   return (
     <>
@@ -15,18 +35,24 @@ export default function Index() {
       <h2>Status</h2>
       {statusData.status}
       {statusData.inputs && (
-        <ul>
-          {statusData.inputs.map((input, index) => (
-            <>
-              <li>
-                outpoint: {input.outPoint}
+        <>
+          <ul>
+            {statusData.inputs.map((input, index) => (
+              <>
+                <li>
+                  outpoint: {input.outPoint}
+                  <br />
+                  amount: {input.output.value}
+                </li>
                 <br />
-                amount: {input.output.value}
-              </li>
-              <br />
-            </>
-          ))}
-        </ul>
+              </>
+            ))}
+          </ul>
+          <button onClick={handleCancelCoins}>CANCEL CHANNEL OPEN</button>
+          <br />
+          <br />
+          {cancelCoinsError && <div className="danger">{cancelCoinsError}</div>}
+        </>
       )}
     </>
   );

2d0af68ccafdfa81cae7bcf6690ffd224571d038
deregs <deregs@protonmail.com>
2022-06-12 17:28:41 -0400

    add homepage link to header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,15 @@
 import Vortex from '../assets/vortex.svg';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div style={{ fontSize: 30, color: 'white', display: 'flex', flex: 1 }}>
-      <Vortex width="45" height="45" viewBox="0 0 21 25" />
-      LN-VORTEX
-      <br />
+    <div>
+      <Link href="/">
+        <a className="header" style={{ display: 'flex', alignItems: 'center' }}>
+          <Vortex width="45" height="45" viewBox="0 0 21 25" />
+          <span>LN-VORTEX</span>
+        </a>
+      </Link>
       <br />
     </div>
   );

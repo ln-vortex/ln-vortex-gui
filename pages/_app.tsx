260c49e60617579e9eb49e66c84d1b9f3d9cbec7
deregs <deregs@protonmail.com>
2022-06-20 22:14:59 -0400

    convert project to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/styles.css';
 import '../styles/fonts.css';
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
--- pages/create.tsx
+++ pages/create.tsx
@@ -8,7 +8,9 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function Create() {
   const { data: utxoList, error: utxoError } = useSWR('/api/utxos', fetcher);
   const { data: status, error: statusError } = useSWR('/api/status', fetcher);
-  const [checkedState, setCheckedState] = useState();
+  const [checkedState, setCheckedState] = useState<Array<boolean> | undefined>(
+    undefined
+  );
   const [satsSelected, setSatsSelected] = useState(0);
   const [nodePubkey, setNodePubkey] = useState('');
   const [host, setHost] = useState('');

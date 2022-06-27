8d7c6948154094ca129b1211152e7b5d9ae4acb1
deregs <deregs@protonmail.com>
2022-06-26 22:00:39 -0400

    add getinfo and getbalance calls

---
+++ pages/api/getbalance/index.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+  await axios
+    .post(
+      process.env.rpcUrl,
+      {
+        jsonrpc: '2.0',
+        method: 'getbalance',
+        id: +new Date(),
+      },
+      {
+        auth: {
+          username: process.env.rpcUser,
+          password: process.env.rpcPassword,
+        },
+      }
+    )
+    .then(({ data }) => {
+      res.status(200).json(data.result);
+    })
+    .catch(({ response }) => {
+      res.status(400).json(response.data);
+    });
+}

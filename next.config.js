3e2351c43cc017652c6eae64a1c371b1c22a95af
benthecarman <benthecarman@live.com>
2022-09-23 02:16:28 -0500

    Rename env variables to be vortex specific

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
-    rpcUrl: process.env.RPC_URL,
-    rpcUser: process.env.RPC_USER,
-    rpcPassword: process.env.RPC_PASSWORD,
+    rpcUrl: process.env.VORTEX_RPC_URL,
+    rpcUser: process.env.VORTEX_RPC_USER,
+    rpcPassword: process.env.VORTEX_RPC_PASSWORD,
   },
 };
